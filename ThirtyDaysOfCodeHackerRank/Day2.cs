688caecceb8f61a21f416837402d25b503c84fcf
Md Layek <layek2883@diu.edu.bd>
2022-08-16 19:58:07 +0600

    Day 4 problem solution

--- ThirtyDaysOfCodeHackerRank/Day2.cs
+++ ThirtyDaysOfCodeHackerRank/Day2.cs
@@ -1,4 +1,4 @@
-﻿namespace ThirtyDaysOfCodeHackerRank;
+﻿// namespace ThirtyDaysOfCodeHackerRank;
 
 /*
 public class Day2
@@ -13,36 +13,42 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Solution {
-    static void Main(String[] args) {
+class Solution
+{
+    static void Main(String[] args)
+    {
         int i = 4;
         double d = 4.0;
         string s = "HackerRank ";
 
-        
+
         // Declare second integer, double, and String variables.
-        
+
         int i2;
         double d2;
         string s2;
-        
+
         // Read and save an integer, double, and String to your variables.
-        i2=int.Parse(Console.ReadLine());
-        d2=double.Parse(Console.ReadLine());
-        s2=Console.ReadLine();
+        i2 = int.Parse(Console.ReadLine());
+        d2 = double.Parse(Console.ReadLine());
+        s2 = Console.ReadLine();
 
         // Print the sum of both integer variables on a new line.
-        Console.WriteLine(String.Format("{0:0}",i+i2));
-                
-        
-        // Print the sum of the double variables on a new line.
-        Console.WriteLine(String.Format("{0:0.0}",d+d2));
+        Console.WriteLine(String.Format("{0:0}", i + i2));
+        // Console.WriteLine($"{i+i2}");
 
-        
+
+        // Print the sum of the double variables on a new line.
+        Console.WriteLine(String.Format("{0:0.0}", d + d2));
+        // Console.WriteLine($"{d+d2}");
+
+
+
         // Concatenate and print the String variables on a new line
         // The 's' variable above should be printed first.
-        
-        Console.WriteLine(s+s2);
 
+        Console.WriteLine(s + s2);
 
     }
+
+}
