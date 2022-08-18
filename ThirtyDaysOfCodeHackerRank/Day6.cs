f6bf9959b379f4fad21b03b780a8efabaf16ac70
Md Layek <layek2883@diu.edu.bd>
2022-08-18 19:37:43 +0600

    day 6 updated back

--- ThirtyDaysOfCodeHackerRank/Day6.cs
+++ ThirtyDaysOfCodeHackerRank/Day6.cs
@@ -27,7 +27,7 @@ class Solution
     public static void Main(string[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
-        for(int i=1;i<11;i++){
+        for(int i=1;i<=10;i++){
             Console.WriteLine($"{n} x {i} = {n * i}");
         }
     }
