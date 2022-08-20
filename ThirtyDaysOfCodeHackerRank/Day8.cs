e71a32f962a016daa619ca3cf64d3a6a3e579783
Md Layek <layek2883@diu.edu.bd>
2022-08-21 01:23:24 +0600

    Day 8 challenge updated

--- ThirtyDaysOfCodeHackerRank/Day8.cs
+++ ThirtyDaysOfCodeHackerRank/Day8.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System.CodeDom.Compiler;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
 using System.ComponentModel;
@@ -24,11 +22,13 @@ class Solution
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
         
+        
         for(var i=n-1; i>-1; i--){
             
             Console.Write(arr[i] + " ");
             
         }
+        
         
     }
 }
