b766c307406d6c745a28169d1a758dcf4247b9c5
Md Layek <layek2883@diu.edu.bd>
2022-08-21 01:11:52 +0600

    Day8 problem solved

---
+++ ThirtyDaysOfCodeHackerRank/Day7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        int N=int.Parse(Console.ReadLine());
+        for(int i=0; i < N; i++){
+            string myString=Console.ReadLine();
+            
+            for(int j=0;j<myString.Length;j++){
+                
+                if(j%2 = =0) Console.Write(myString[j]);
+                
+            }
+            
+            Console.Write(" ");
+            
+            for(int j=0;j<myString.Length;j++){
+                if(j%2 !=0;j<myString.Length;j++) Console.Write(myString[j]);
+            }
+            
+            Console.Write(Environment.NewLine);
+        }
+        
+        
+    }
+}
---
+++ ThirtyDaysOfCodeHackerRank/Day8.cs
@@ -0,0 +1,42 @@
+﻿// namespace ThirtyDaysOfCodeHackerRank;
+//
+// public class Day8
+// {
+//     
+// }
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+        // int[] arr =Array.ConvertAll(arr,Int32.parse);
+        
+        for(var i=n-1; i>-1; i--){
+            
+            Console.Write(arr[i] + " ");
+            
+        }
+        
+        
+    }
+}
