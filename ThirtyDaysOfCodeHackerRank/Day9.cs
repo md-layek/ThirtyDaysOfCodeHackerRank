72f1d03ba01f53afbd8050c55905ebd42379e6dc
Md Layek <layek2883@diu.edu.bd>
2022-08-22 21:23:59 +0600

    Day 9 solution added

---
+++ ThirtyDaysOfCodeHackerRank/Day9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        var n=int.Parse(Console.ReadLine());
+        var phoneBook=new Dictionary<string, int>();
+        for(var i=0;i < n; i++){
+            var entry=Console.ReadLine().Split(' ');
+            var name=entry[0];
+            var phone=int.Parse(entry[1]);
+            phoneBook.Add(name, phone);
+        }
+        for(var i=0;i<n;i++){
+            var name=Console.ReadLine();
+            if(phoneBook.ContainsKey(name)){
+                var phone=phoneBook[name];
+                Console.WriteLine($"{name}={phone}");
+            }
+            else Console.WriteLine("Not found");
+        }
+    }
+}
+
+
