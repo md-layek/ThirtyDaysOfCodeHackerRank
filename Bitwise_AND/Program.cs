49f38ef238c787376d78069f98c07707b9f162e4
Md Layek <layek2883@diu.edu.bd>
2022-09-12 17:13:48 +0600

    Day 30 Bitwise_And solution added

---
+++ Bitwise_AND/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+using Bitwise_AND;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int count = Convert.ToInt32(firstMultipleInput[0]);
+
+            int lim = Convert.ToInt32(firstMultipleInput[1]);
+
+            int res = Result.bitwiseAnd(count, lim);
+
+            textWriter.WriteLine(res);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Bitwise_AND/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace Bitwise_AND;
+
+public class Result
+{
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K)
+    {
+        
+        int i,j,maxResult;
+        maxResult=K-1;
+        while(maxResult>0) {            
+            for( i=N;i>0;i--) {
+                for( j=i-1;j>0;j--) {
+                    if((i&j) == maxResult ) return maxResult;
+                }
+            }
+            maxResult--;
+        }
+        return 0;
+       
+    }
+
+}
